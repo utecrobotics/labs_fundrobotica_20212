63b82a5837535bfd7a42680b521404aa0e241e01
Alexander López <lopez.alexander@pucp.edu.pe>
2021-10-02 13:08:09 -0500

    Add files via upload

---
+++ lab3/src/lab3functions.py
@@ -0,0 +1,35 @@
+import numpy as np
+from copy import copy
+
+cos=np.cos; sin=np.sin; pi=np.pi
+
+
+def dh(d, theta, a, alpha):
+    """
+    Calcular la matriz de transformacion homogenea asociada con los parametros
+    de Denavit-Hartenberg.
+    Los valores d, theta, a, alpha son escalares.
+    """
+    # Escriba aqui la matriz de transformacion homogenea en funcion de los valores de d, theta, a, alpha
+    T = 0
+    return T
+    
+    
+
+def fkine_ur5(q):
+    """
+    Calcular la cinematica directa del robot UR5 dados sus valores articulares. 
+    q es un vector numpy de la forma [q1, q2, q3, q4, q5, q6]
+    """
+    # Longitudes (en metros)
+
+    # Matrices DH (completar), emplear la funcion dh con los parametros DH para cada articulacion
+    T1 = 0
+    T2 = 0
+    T3 = 0
+    T4 = 0
+    T5 = 0
+    T6 = 0
+    # Efector final con respecto a la base
+    T = 0
+    return T
